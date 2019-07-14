7a078a2bca24c3df5e2369e7a11aa46f53e5e0a9
Tyler Sefcik <tyler.sefcik@gmail.com>
2019-07-14 16:35:59 -0400

    no mas

--- project/DecisionTree.py
+++ project/DecisionTree.py
@@ -3,30 +3,45 @@ from project import Util as util
 import pandas as pd
 
 """
-This class represents the Decision Tree that will be built using the ID3 Machine Learning algorithm.  It takes three 
-parameters: data = data set, features = list of features in the data set, target_class = name of target class. 
+This class represents the Decision Tree that will be built using the ID3 Machine Learning algorithm.  It takes two 
+parameters: data = data set and features = list of features in the data set. 
 """
 
 
 class DecisionTree:
+    def __init__(self):
+        self.prediction = None
 
     def create_decision_tree(self, data, features_list):
+        # Initialize a list to keep the class values in
         class_list_from_data = []
 
+        # Add all class values to the above list
         for index, row in data.iterrows():
             class_list_from_data.append(data.iloc[index, data.shape[1] - 1])
 
+        # Get unique classes (returns the different types of classes in this particular data set
         unique_classes = set(class_list_from_data)
 
-        if len(unique_classes) == 1 or len(features_list) == 1:
-            new_node = node.Node(data=data, feature=-1, leaf_node=True)
+        # We know we have a leaf node if there is only one class value in the class list
+        # We know we have a leaf node if there is only one feature left in the feature list
+        if len(unique_classes) == 1 or len(features_list) == 2:
+            most_common_class = util.most_common_class(class_list_from_data)
+            # Create leaf node since there will not be any children attached at this point
+            # -99 represents a leaf node since no feature is attached
+            new_node = node.Node(data=data, feature=most_common_class, leaf_node=True)
+            self.prediction = most_common_class
             return new_node
 
+        # Get best feature left in this data
         best_feature_available = util.select_best_feature_by_information_gain(data=data)
 
+        # Make a copy of the original feature list before removing it from the feature list since we will need it below
         features_list_original = features_list
+        # Remove best available feature since we will not be using it again
         features_list.remove(best_feature_available)
 
+        # Create new node that will have children since it is not a leaf node
         new_node = node.Node(data=data, feature=best_feature_available, leaf_node=False)
 
         best_feature_available_values = []
@@ -38,22 +53,80 @@ class DecisionTree:
 
         for value in unique_best_feature_values:
             value_data = pd.DataFrame(columns=features_list_original)
-            value_data = value_data.append(data[data[best_feature_available] == value], ignore_index=True)
+            value_data = value_data.append(data[data[best_feature_available] == value], ignore_index=True, sort=True)
             value_data.drop(best_feature_available, axis=1, inplace=True)
             class_col = value_data.pop("class")
             value_data["class"] = class_col
-            print(value_data)
             new_node.children[value] = self.create_decision_tree(data=value_data, features_list=features_list)
 
         return new_node
 
-    def get_tree_as_string(self, node2):
-        if node2.leaf_node:
-            string = '{' + str(node2.feature) + '}'
-            return string
+    # Run test with data set to predict accuracy
+    def run_test(self, data, tree):
+        # Keep list of predictions
+        predictions = []
+        # Initialize a list to keep the class values in
+        class_list_from_data = []
+
+        # Add all class values to the above list
+        for index, row in data.iterrows():
+            class_list_from_data.append(data.iloc[index, data.shape[1] - 1])
+            # Set current node to variable
+            curr_node = tree
+
+            # Loop until we hit a leaf node
+            while not curr_node.leaf_node:
+                # Get feature value at current row/feature
+                feature_val = data.iloc[index].loc[curr_node.feature]
+
+                # Set children variable with current nodes children since we need to look through them
+                curr_node_children = curr_node.children
+                # If the feature value is not a part of the current node in some way, break out of the loop, we hit a
+                # leaf node
+                if feature_val not in curr_node_children:
+                    break
+
+                # Set current node to child node where the feature matches
+                curr_node = curr_node.children[feature_val]
+                self.prediction = curr_node.feature
+
+            # Get prediction by most popular class from that node to be used for comparison later
+            prediction = self.prediction
+            predictions.append(prediction)
+
+        # Compare the actual classes to the predicted classes found by iterating through the tree
+        total_correct = 0
+        for index in range(0, len(predictions) - 1):
+            if predictions[index] == class_list_from_data[index]:
+                total_correct += 1
+
+        # Accuracy of the decision tree on all rows in data
+        accuracy = (total_correct/len(predictions))*100
+
+        return accuracy
+
+    # Prune Tree
+    def prune_tree(self, data, tree):
+        # Consider pruning as long as we are not on a leaf node
+        if not tree.leaf_node:
+            # Iterate through children nodes and recursively call prune_tree (will stop once we hit a leaf node)
+            for value, child in tree.children.items():
+                self.prune_tree(data, child)
+
+            # Get the original score
+            original_score = self.run_test(data, tree)
+
+            # Make this node a leaf node and get rid of children if there were some before this
+            tree.leaf_node = True
+            tree_children = tree.children
+            tree.children = {}
+            # Get updated score
+            updated_score = self.run_test(data, tree)
+
+            # Compare score
+            if original_score > updated_score:
+                tree.leaf_node = False
+                tree.children = tree_children
+            return tree
         else:
-            string = ' { ' + str(node2.feature) + ': '
-            for child_key in node2.children:
-                string = string + self.get_tree_as_string(node2.children[child_key])
-            string = string + ' } '
-        return string
+            return tree
--- project/Main.py
+++ project/Main.py
@@ -3,22 +3,26 @@ from project import Abalone as a
 from project import Car as c
 from project import Segmentation as s
 from project import FiveFold as ff
-from project import Util as util
 from project import DecisionTree as dt
+import numpy as np
 import sys
 
 """
-This is the main driver class for Assignment#3.  @author: Tyler Sefcik
+This is the main driver class for Assignment#4.  @author: Tyler Sefcik
 """
 
 
 class Main:
 
     # Segmentation setup and run
-    def run_seg(self, filename, column_names, sortby):
+    def run_seg(self, filename, sortby):
+        print()
+        seg_names = ["class", "cen col", "cen row", "pix count", "sld -5", "sld -2", "vedge mean", "vedge sd",
+                     "hedge mean", "hedge sd", "intensity mean", "rawred mean", "rawblue mean", "rawgreen mean",
+                     "exred mean", "exblue mean", "exgreen mean", "value mean", "sat mean", "hue mean"]
         # Setup data
         seg = s.Segmentation()
-        seg_data = seg.setup_data(filename=filename, column_names=column_names)
+        seg_data = seg.setup_data(filename=filename, column_names=seg_names)
         # Split the data set into 10% and 90%
         seg_validation_data = seg_data.sample(frac=.10)
         seg_data_rest = seg_data.drop(seg_validation_data.index)
@@ -26,21 +30,88 @@ class Main:
         seg_validation_data.reset_index(inplace=True)
         seg_data_rest.reset_index(inplace=True)
 
-        print(seg_validation_data)
-        print()
-        print(seg_data_rest)
-
         # Setup five fold cross validation
         five_fold = ff.FiveFold()
         seg1, seg2, seg3, seg4, seg5 = five_fold.five_fold_sort_class(data=seg_data_rest, sortby=sortby)
+        seg1.drop(columns='index', axis=1, inplace=True)
+        seg2.drop(columns='index', axis=1, inplace=True)
+        seg3.drop(columns='index', axis=1, inplace=True)
+        seg4.drop(columns='index', axis=1, inplace=True)
+        seg5.drop(columns='index', axis=1, inplace=True)
 
-        return seg_data
+        tree1 = dt.DecisionTree()
+        tree_node1 = tree1.create_decision_tree(data=seg1, features_list=seg_names)
+        accuracy1 = tree1.run_test(seg_validation_data, tree_node1)
+        print("Unpruned accuracy for seg1: " + str(accuracy1) + "%")
+        tree_pruned1 = tree1.prune_tree(seg_validation_data, tree_node1)
+        accuracy_pruned1 = tree1.run_test(seg_validation_data, tree_pruned1)
+        print("Pruned accuracy for seg1: " + str(accuracy_pruned1) + "%")
+        print()
+
+        seg_names = ["class", "cen col", "cen row", "pix count", "sld -5", "sld -2", "vedge mean", "vedge sd",
+                     "hedge mean", "hedge sd", "intensity mean", "rawred mean", "rawblue mean", "rawgreen mean",
+                     "exred mean", "exblue mean", "exgreen mean", "value mean", "sat mean", "hue mean"]
+        tree2 = dt.DecisionTree()
+        tree_node2 = tree2.create_decision_tree(data=seg2, features_list=seg_names)
+        accuracy2 = tree2.run_test(seg_validation_data, tree_node2)
+        print("Unpruned accuracy for seg2: " + str(accuracy2) + "%")
+        tree_pruned2 = tree2.prune_tree(seg_validation_data, tree_node2)
+        accuracy_pruned2 = tree2.run_test(seg_validation_data, tree_pruned2)
+        print("Pruned accuracy for seg2: " + str(accuracy_pruned2) + "%")
+        print()
+
+        seg_names = ["class", "cen col", "cen row", "pix count", "sld -5", "sld -2", "vedge mean", "vedge sd",
+                     "hedge mean", "hedge sd", "intensity mean", "rawred mean", "rawblue mean", "rawgreen mean",
+                     "exred mean", "exblue mean", "exgreen mean", "value mean", "sat mean", "hue mean"]
+        tree3 = dt.DecisionTree()
+        tree_node3 = tree3.create_decision_tree(data=seg3, features_list=seg_names)
+        accuracy3 = tree3.run_test(seg_validation_data, tree_node3)
+        print("Unpruned accuracy for seg3: " + str(accuracy3) + "%")
+        tree_pruned3 = tree3.prune_tree(seg_validation_data, tree_node3)
+        accuracy_pruned3 = tree3.run_test(seg_validation_data, tree_pruned3)
+        print("Pruned accuracy for seg3: " + str(accuracy_pruned3) + "%")
+        print()
+
+        seg_names = ["class", "cen col", "cen row", "pix count", "sld -5", "sld -2", "vedge mean", "vedge sd",
+                     "hedge mean", "hedge sd", "intensity mean", "rawred mean", "rawblue mean", "rawgreen mean",
+                     "exred mean", "exblue mean", "exgreen mean", "value mean", "sat mean", "hue mean"]
+        tree4 = dt.DecisionTree()
+        tree_node4 = tree4.create_decision_tree(data=seg4, features_list=seg_names)
+        accuracy4 = tree4.run_test(seg_validation_data, tree_node4)
+        print("Unpruned accuracy for seg4: " + str(accuracy4) + "%")
+        tree_pruned4 = tree4.prune_tree(seg_validation_data, tree_node4)
+        accuracy_pruned4 = tree4.run_test(seg_validation_data, tree_pruned4)
+        print("Pruned accuracy for seg4: " + str(accuracy_pruned4) + "%")
+        print()
+
+        seg_names = ["class", "cen col", "cen row", "pix count", "sld -5", "sld -2", "vedge mean", "vedge sd",
+                     "hedge mean", "hedge sd", "intensity mean", "rawred mean", "rawblue mean", "rawgreen mean",
+                     "exred mean", "exblue mean", "exgreen mean", "value mean", "sat mean", "hue mean"]
+        tree5 = dt.DecisionTree()
+        tree_node5 = tree5.create_decision_tree(data=seg5, features_list=seg_names)
+        accuracy5 = tree5.run_test(seg_validation_data, tree_node5)
+        print("Unpruned accuracy for seg5: " + str(accuracy5) + "%")
+        tree_pruned5 = tree5.prune_tree(seg_validation_data, tree_node5)
+        accuracy_pruned5 = tree5.run_test(seg_validation_data, tree_pruned5)
+        print("Pruned accuracy for seg5: " + str(accuracy_pruned5) + "%")
+        print()
+
+        unpruned_accuracy_average = np.average([accuracy1, accuracy2, accuracy3, accuracy4, accuracy5])
+        pruned_accuracy_average = np.average([accuracy_pruned1, accuracy_pruned2, accuracy_pruned3, accuracy_pruned4,
+                                             accuracy_pruned5])
+
+        print("Unpruned accuracy average for seg data: " + str(unpruned_accuracy_average) + "%")
+        print("Pruned accuracy average for seg data: " + str(pruned_accuracy_average) + "%")
+        print()
 
     # Abalone setup and run
-    def run_abalone(self, filename, column_names, sortby):
+    def run_abalone(self, filename, sortby):
+        print()
+        abalone_names = ["sex", "length", "diameter", "height", "whole weight", "shucked weight", "viscera weight",
+                         "shell weight", "class"]
         # Setup data
         abalone = a.Abalone()
-        abalone_data = abalone.setup_data(filename=filename, column_names=column_names)
+        abalone_data = abalone.setup_data(filename=filename, column_names=abalone_names)
         # Split the data set into 10% and 90%
         abalone_validation_data = abalone_data.sample(frac=.10)
         abalone_data_rest = abalone_data.drop(abalone_validation_data.index)
@@ -48,22 +119,79 @@ class Main:
         abalone_validation_data.reset_index(inplace=True)
         abalone_data_rest.reset_index(inplace=True)
 
-        print(abalone_validation_data)
-        print()
-        print(abalone_data_rest)
-
         # Setup five fold cross validation
         five_fold = ff.FiveFold()
         abalone1, abalone2, abalone3, abalone4, abalone5 = five_fold.five_fold_sort_class(data=abalone_data,
                                                                                           sortby=sortby)
 
-        return abalone_data
+        tree1 = dt.DecisionTree()
+        tree_node1 = tree1.create_decision_tree(data=abalone1, features_list=abalone_names)
+        accuracy1 = tree1.run_test(abalone_validation_data, tree_node1)
+        print("Unpruned accuracy for abalone1: " + str(accuracy1) + "%")
+        tree_pruned1 = tree1.prune_tree(abalone_validation_data, tree_node1)
+        accuracy_pruned1 = tree1.run_test(abalone_validation_data, tree_pruned1)
+        print("Pruned accuracy for abalone1: " + str(accuracy_pruned1) + "%")
+        print()
+
+        abalone_names = ["sex", "length", "diameter", "height", "whole weight", "shucked weight", "viscera weight",
+                         "shell weight", "class"]
+        tree2 = dt.DecisionTree()
+        tree_node2 = tree2.create_decision_tree(data=abalone2, features_list=abalone_names)
+        accuracy2 = tree2.run_test(abalone_validation_data, tree_node2)
+        print("Unpruned accuracy for abalone2: " + str(accuracy2) + "%")
+        tree_pruned2 = tree2.prune_tree(abalone_validation_data, tree_node2)
+        accuracy_pruned2 = tree2.run_test(abalone_validation_data, tree_pruned2)
+        print("Pruned accuracy for abalone2: " + str(accuracy_pruned2) + "%")
+        print()
+
+        abalone_names = ["sex", "length", "diameter", "height", "whole weight", "shucked weight", "viscera weight",
+                         "shell weight", "class"]
+        tree3 = dt.DecisionTree()
+        tree_node3 = tree3.create_decision_tree(data=abalone3, features_list=abalone_names)
+        accuracy3 = tree3.run_test(abalone_validation_data, tree_node3)
+        print("Unpruned accuracy for abalone3: " + str(accuracy3) + "%")
+        tree_pruned3 = tree3.prune_tree(abalone_validation_data, tree_node3)
+        accuracy_pruned3 = tree3.run_test(abalone_validation_data, tree_pruned3)
+        print("Pruned accuracy for abalone3: " + str(accuracy_pruned3) + "%")
+        print()
+
+        abalone_names = ["sex", "length", "diameter", "height", "whole weight", "shucked weight", "viscera weight",
+                         "shell weight", "class"]
+        tree4 = dt.DecisionTree()
+        tree_node4 = tree4.create_decision_tree(data=abalone4, features_list=abalone_names)
+        accuracy4 = tree4.run_test(abalone_validation_data, tree_node4)
+        print("Unpruned accuracy for abalone4: " + str(accuracy4) + "%")
+        tree_pruned4 = tree4.prune_tree(abalone_validation_data, tree_node4)
+        accuracy_pruned4 = tree4.run_test(abalone_validation_data, tree_pruned4)
+        print("Pruned accuracy for abalone4: " + str(accuracy_pruned4) + "%")
+        print()
+
+        abalone_names = ["sex", "length", "diameter", "height", "whole weight", "shucked weight", "viscera weight",
+                         "shell weight", "class"]
+        tree5 = dt.DecisionTree()
+        tree_node5 = tree5.create_decision_tree(data=abalone5, features_list=abalone_names)
+        accuracy5 = tree5.run_test(abalone_validation_data, tree_node5)
+        print("Unpruned accuracy for abalone5: " + str(accuracy5) + "%")
+        tree_pruned5 = tree5.prune_tree(abalone_validation_data, tree_node5)
+        accuracy_pruned5 = tree5.run_test(abalone_validation_data, tree_pruned5)
+        print("Pruned accuracy for abalone5: " + str(accuracy_pruned5) + "%")
+        print()
+
+        unpruned_accuracy_average = np.average([accuracy1, accuracy2, accuracy3, accuracy4, accuracy5])
+        pruned_accuracy_average = np.average([accuracy_pruned1, accuracy_pruned2, accuracy_pruned3, accuracy_pruned4,
+                                             accuracy_pruned5])
+
+        print("Unpruned accuracy average for abalone data: " + str(unpruned_accuracy_average) + "%")
+        print("Pruned accuracy average for abalone data: " + str(pruned_accuracy_average) + "%")
+        print()
 
     # Car setup and run
-    def run_car(self, filename, column_names, sortby):
+    def run_car(self, filename, sortby):
+        print()
+        car_names = ["buying", "maint", "doors", "persons", "lug boot", "safety", "class"]
         # Setup data
         car = c.Car()
-        car_data = car.setup_data(filename=filename, column_names=column_names)
+        car_data = car.setup_data(filename=filename, column_names=car_names)
         # Split the data set into 10% and 90%
         car_validation_data = car_data.sample(frac=.10)
         car_data_rest = car_data.drop(car_validation_data.index)
@@ -77,9 +205,69 @@ class Main:
 
         # Setup five fold cross validation
         five_fold = ff.FiveFold()
-        car1, car2, car3, car4, car5 = five_fold.five_fold_sort_class(data=car_data, sortby=sortby)
+        car1, car2, car3, car4, car5 = five_fold.five_fold_sort_class(data=car_data_rest, sortby=sortby)
+        car1.drop(columns='index', axis=1, inplace=True)
+        car2.drop(columns='index', axis=1, inplace=True)
+        car3.drop(columns='index', axis=1, inplace=True)
+        car4.drop(columns='index', axis=1, inplace=True)
+        car5.drop(columns='index', axis=1, inplace=True)
 
-        return car_data
+        tree1 = dt.DecisionTree()
+        tree_node1 = tree1.create_decision_tree(data=car1, features_list=car_names)
+        accuracy1 = tree1.run_test(car_validation_data, tree_node1)
+        print("Unpruned accuracy for car1: " + str(accuracy1) + "%")
+        tree_pruned1 = tree1.prune_tree(car_validation_data, tree_node1)
+        accuracy_pruned1 = tree1.run_test(car_validation_data, tree_pruned1)
+        print("Pruned accuracy for car1: " + str(accuracy_pruned1) + "%")
+        print()
+
+        car_names = ["buying", "maint", "doors", "persons", "lug boot", "safety", "class"]
+        tree2 = dt.DecisionTree()
+        tree_node2 = tree2.create_decision_tree(data=car2, features_list=car_names)
+        accuracy2 = tree2.run_test(car_validation_data, tree_node2)
+        print("Unpruned accuracy for car2: " + str(accuracy2) + "%")
+        tree_pruned2 = tree2.prune_tree(car_validation_data, tree_node2)
+        accuracy_pruned2 = tree2.run_test(car_validation_data, tree_pruned2)
+        print("Pruned accuracy for car2: " + str(accuracy_pruned2) + "%")
+        print()
+
+        car_names = ["buying", "maint", "doors", "persons", "lug boot", "safety", "class"]
+        tree3 = dt.DecisionTree()
+        tree_node3 = tree3.create_decision_tree(data=car3, features_list=car_names)
+        accuracy3 = tree3.run_test(car_validation_data, tree_node3)
+        print("Unpruned accuracy for car3: " + str(accuracy3) + "%")
+        tree_pruned3 = tree3.prune_tree(car_validation_data, tree_node3)
+        accuracy_pruned3 = tree3.run_test(car_validation_data, tree_pruned3)
+        print("Pruned accuracy for car3: " + str(accuracy_pruned3) + "%")
+        print()
+
+        car_names = ["buying", "maint", "doors", "persons", "lug boot", "safety", "class"]
+        tree4 = dt.DecisionTree()
+        tree_node4 = tree4.create_decision_tree(data=car4, features_list=car_names)
+        accuracy4 = tree4.run_test(car_validation_data, tree_node4)
+        print("Unpruned accuracy for car4: " + str(accuracy4) + "%")
+        tree_pruned4 = tree4.prune_tree(car_validation_data, tree_node4)
+        accuracy_pruned4 = tree4.run_test(car_validation_data, tree_pruned4)
+        print("Pruned accuracy for car4: " + str(accuracy_pruned4) + "%")
+        print()
+
+        car_names = ["buying", "maint", "doors", "persons", "lug boot", "safety", "class"]
+        tree5 = dt.DecisionTree()
+        tree_node5 = tree5.create_decision_tree(data=car5, features_list=car_names)
+        accuracy5 = tree5.run_test(car_validation_data, tree_node5)
+        print("Unpruned accuracy for car5: " + str(accuracy5) + "%")
+        tree_pruned5 = tree5.prune_tree(car_validation_data, tree_node5)
+        accuracy_pruned5 = tree5.run_test(car_validation_data, tree_pruned5)
+        print("Pruned accuracy for car5: " + str(accuracy_pruned5) + "%")
+        print()
+
+        unpruned_accuracy_average = np.average([accuracy1, accuracy2, accuracy3, accuracy4, accuracy5])
+        pruned_accuracy_average = np.average([accuracy_pruned1, accuracy_pruned2, accuracy_pruned3, accuracy_pruned4,
+                                             accuracy_pruned5])
+
+        print("Unpruned accuracy average for car data: " + str(unpruned_accuracy_average) + "%")
+        print("Pruned accuracy average for car data: " + str(pruned_accuracy_average) + "%")
+        print()
 
     # Main driver to run all algorithms on each dataset
     def main(self):
@@ -87,37 +275,14 @@ class Main:
         # Comment out for printing in console
         # sys.stdout = open("./Assignment4Output.txt", "w")
 
+        # ##### Car #####
+        # self.run_car(filename="data/car.data", sortby="class")
+        #
         # ##### Segmentation #####
-        # seg_names = ["class", "cen col", "cen row", "pix count", "sld -5", "sld -2", "vedge mean", "vedge sd",
-        #              "hedge mean", "hedge sd", "intensity mean", "rawred mean", "rawblue mean", "rawgreen mean",
-        #              "exred mean", "exblue mean", "exgreen mean", "value mean", "sat mean", "hue mean"]
-        # seg_data = self.run_seg(filename="data/segmentation.data", column_names=seg_names, sortby="class")
-        #
-        # print("Segmentation Data")
-        # print(seg_data)
-        # print()
-        #
-        # ##### Abalone #####
-        # abalone_names = ["sex", "length", "diameter", "height", "whole weight", "shucked weight", "viscera weight",
-        #                  "shell weight", "class"]
-        # abalone_data = self.run_abalone(filename="data/abalone.data", column_names=abalone_names, sortby="class")
-        #
-        # print("Abalone Data")
-        # print(abalone_data)
-        # print()
+        # self.run_seg(filename="data/segmentation.data", sortby="class")
 
-        ##### Car #####
-        car_names = ["buying", "maint", "doors", "persons", "lug boot", "safety", "class"]
-        car_data = self.run_car(filename="data/car.data", column_names=car_names, sortby="class")
-
-        print("Car Data")
-        print(car_data)
-        print()
-
-        tree = dt.DecisionTree()
-        tree_node = tree.create_decision_tree(data=car_data, features_list=car_names)
-        # tree_as_string = tree.get_tree_as_string(tree_node)
-        # print(tree_as_string)
+        ##### Abalone #####
+        self.run_abalone(filename="data/abalone.data", sortby="class")
 
 
 if __name__ == "__main__":
